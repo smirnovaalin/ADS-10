d5aaf461b5d744cab41431f221914f6d60781143
smirnovaalin <160322763+smirnovaalin@users.noreply.github.com>
2024-06-09 10:43:29 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,54 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+struct Node {
+	char elem;
+	std::vector<Node*> childs;
+    explicit Node(char c) : elem(c) {}
+};
+
+class Tree {
+private:
+	std::vector <std::vector<char>> perm;
+    Node* root;
+
+    void buildTree(const std::vector<char>& symbol, Node* parent) {
+        for (char c : symbol) {
+            Node* child = new Node(c);
+            parent->childs.push_back(child);
+            std::vector<char> remainingSymbol(symbol);
+            remainingSymbol.erase(std::find(remainingSymbol.begin(),
+                remainingSymbol.end(), c));
+            buildTree(remainingSymbol, child);
+        }
+    }
+
+    void generatePermut(Node* data, std::vector<char>* dataPermutation,
+        std::vector<std::vector<char>>* permutations) const {
+        dataPermutation->push_back(data->elem);
+        if (data->childs.empty()) {
+            permutations->push_back(*dataPermutation);
+        }
+        else {
+            for (Node* child : data->childs) {
+                generatePermut(child, dataPermutation, permutations);
+            }
+        }
+        dataPermutation->pop_back();
+    }
+
+public:
+	std::vector<std::vector<char> > getPerm() const {
+		return perm;
+	}
+    explicit Tree(const std::vector<char>& symbol) {
+        root = new Node('\0');
+        buildTree(symbol, root);
+    }
+};
 #endif  // INCLUDE_TREE_H_
