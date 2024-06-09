d819920481a57fa90bcccb0e9e97a9122bfb79f6
smirnovaalin <160322763+smirnovaalin@users.noreply.github.com>
2024-06-09 11:05:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,11 +6,11 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-   std::vector<std::vector<char>> pr = tree.getPerm();
-    if (n < 0 || pr.size() < n) {
-        return {};
-    }
-    std::vector<char> temp = pr[n - 1];
-    temp.erase(temp.begin());
-    return temp;
+  std::vector<std::vector<char>> pr = tree.getPerm();
+  if (n < 0 || pr.size() < n) {
+    return {};
+  }
+  std::vector<char> temp = pr[n - 1];
+  temp.erase(temp.begin());
+  return temp;
 }
