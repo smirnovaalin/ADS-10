4902c810cd36eeb1d5ae54a0e6cfe3b5dfaa00ae
smirnovaalin <160322763+smirnovaalin@users.noreply.github.com>
2024-06-09 11:11:27 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -7,50 +7,48 @@
 #include <algorithm>
 
 struct Node {
-	char elem;
-	std::vector<Node*> childs;
-    explicit Node(char c) : elem(c) {}
+  char elem;
+  std::vector<Node*> childs;
+  explicit Node(char c) : elem(c) {}
 };
 
 class Tree {
-public:
-	std::vector<std::vector<char> > getPerm() const {
-        std::vector <std::vector<char>> perm;
-        std::vector<char> current;
-        generatePermut(root, &current, &perm);
-        return perm;
+
+ public:
+  std::vector<std::vector<char> > getPerm() const {
+    std::vector <std::vector<char>> perm;
+    std::vector<char> current;
+    generatePermut(root, &current, &perm);
+    return perm;
 	}
-    explicit Tree(const std::vector<char>& symbol) {
-        root = new Node('\0');
-        buildTree(symbol, root);
-    }
+
+  explicit Tree(const std::vector<char>& symbol) {
+    root = new Node('\0');
+    buildTree(symbol, root);
+  }
+
 private:
-    
-    Node* root;
-
-    void buildTree(const std::vector<char>& symbol, Node* parent) {
-        for (char c : symbol) {
-            Node* child = new Node(c);
-            parent->childs.push_back(child);
-            std::vector<char> remainingSymbol(symbol);
-            remainingSymbol.erase(std::find(remainingSymbol.begin(),
-                remainingSymbol.end(), c));
-            buildTree(remainingSymbol, child);
-        }
+  Node* root;
+  void buildTree(const std::vector<char>& symbol, Node* parent) {
+    for (char c : symbol) {
+      Node* child = new Node(c);
+      parent->childs.push_back(child);
+      std::vector<char> remainingSymbol(symbol);
+      remainingSymbol.erase(std::find(remainingSymbol.begin(),remainingSymbol.end(), c));
+      buildTree(remainingSymbol, child);
     }
+  }
 
-    void generatePermut(Node* data, std::vector<char>* dataPermutation,
-        std::vector<std::vector<char>>* permutations) const {
-        dataPermutation->push_back(data->elem);
-        if (data->childs.empty()) {
-            permutations->push_back(*dataPermutation);
-        }
-        else {
-            for (Node* child : data->childs) {
-                generatePermut(child, dataPermutation, permutations);
-            }
-        }
-        dataPermutation->pop_back();
+  void generatePermut(Node* data, std::vector<char>* dataPermutation, std::vector<std::vector<char>>* permutations) const {
+    dataPermutation->push_back(data->elem);
+    if (data->childs.empty()) {
+      permutations->push_back(*dataPermutation);
+    } else {
+      for (Node* child : data->childs) {
+        generatePermut(child, dataPermutation, permutations);
+      }
     }
+    dataPermutation->pop_back();
+  }
 };
 #endif  // INCLUDE_TREE_H_
